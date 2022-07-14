5d1ec94fe98ad195ffecd66ddcb7a51f12e56c00
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-07-14 09:14:55 -0300

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+print('***************')
+print('Menu de opciones')
+print('***************')
+
+print('1. Suma')
+print('2. Resta')
+print('3. Multiplicacion')
+print('4. Division')
+print('5. Exponente')
+
+opcion = int(input('Introduce la opcion deseada '))
+
+num1 = int(input('Ingrese el PRIMER  numero'))
+num2 = int(input('Ingrese el SEGUNDO numero'))
+
+ 
